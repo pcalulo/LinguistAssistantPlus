66029ba92bb95809040d92ceeee60c74ec51f8a6
Lawrence Patrick Calulo <lugkhast@gmail.com>
2013-10-15 19:29:08 +0800

    Put a border on the box being hovered on

--- src/textgen/la/ui/Box.java
+++ src/textgen/la/ui/Box.java
@@ -2,9 +2,13 @@ package textgen.la.ui;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class Box extends JPanel {
 	JLabel label, concept;
@@ -14,6 +18,39 @@ public class Box extends JPanel {
 		concept = new JLabel();
 		add(label);
 		add(concept);
+		
+		addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				System.out.println("Mouse exited " + label.getText());
+				Box.this.setBorder(new EmptyBorder(0, 0, 0, 0));
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				System.out.println("Mouse entered " + label.getText());
+				Box.this.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 
 	public void setBackgroundColor(int c) {
