bb89b3743e5937434cdf5c0d696e132c40ab348a
Lawrence Patrick Calulo <lugkhast@gmail.com>
2013-10-15 18:44:50 +0800

    Pretty-format UI classes, and remove unused imports

--- src/textgen/la/ui/Box.java
+++ src/textgen/la/ui/Box.java
@@ -1,56 +1,50 @@
 package textgen.la.ui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Box extends JPanel{
+public class Box extends JPanel {
 	JLabel label, concept;
-	
-	public Box()
-	{
+
+	public Box() {
 		label = new JLabel();
 		concept = new JLabel();
 		add(label);
 		add(concept);
 	}
-	
-	public void setBackgroundColor(int c)
-	{
-		switch (c)
-		{
-			case 0: 
-				this.setBackground(new Color(255, 180, 180));
-				break;
-			case 1: 
-				this.setBackground(new Color(180, 255, 180));
-				break;
-			case 2:
-				this.setBackground(new Color(180, 180, 255));
-				break;
-			case 3:
-				this.setBackground(new Color(255, 255, 180));
-				break;
-			case 4:
-				this.setBackground(new Color(255, 180, 255));
-				break;
+
+	public void setBackgroundColor(int c) {
+		switch (c) {
+		case 0:
+			this.setBackground(new Color(255, 180, 180));
+			break;
+		case 1:
+			this.setBackground(new Color(180, 255, 180));
+			break;
+		case 2:
+			this.setBackground(new Color(180, 180, 255));
+			break;
+		case 3:
+			this.setBackground(new Color(255, 255, 180));
+			break;
+		case 4:
+			this.setBackground(new Color(255, 180, 255));
+			break;
 		}
 	}
-	
-	public void setValues (String l, String c)
-	{
+
+	public void setValues(String l, String c) {
 		if (c != null)
-			label.setText(l+": ");
+			label.setText(l + ": ");
 		else
 			label.setText(l);
 		concept.setText(c);
-		
-		concept.setFont(new Font ("Arial", Font.ITALIC, 12));
-		
+
+		concept.setFont(new Font("Arial", Font.ITALIC, 12));
+
 	}
-	
+
 }
