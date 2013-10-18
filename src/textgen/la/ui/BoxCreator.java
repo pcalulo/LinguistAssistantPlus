21e78c529e77e0674a33a80317ba2022e9563d63
Lawrence Patrick Calulo <lugkhast@gmail.com>
2013-10-18 20:53:09 +0800

    Auto-format BoxCreator code

--- src/textgen/la/ui/BoxCreator.java
+++ src/textgen/la/ui/BoxCreator.java
@@ -12,46 +12,40 @@ public class BoxCreator {
 
 	Sentence sentence;
 	JPanel panel;
-	
-	public BoxCreator(Sentence s) 
-	{
+
+	public BoxCreator(Sentence s) {
 		sentence = s;
-		
+
 		panel = new JPanel();
 	}
-	
-	public void displayBoxes() 
-	{
-		//PRINT VALUES
+
+	public void displayBoxes() {
+		// PRINT VALUES
 		ArrayList<Constituent> clauses = sentence.getClauses();
-		for (int i = 0; i<clauses.size(); i++)
-		{
+		for (int i = 0; i < clauses.size(); i++) {
 			Box box = new Box();
 			box.setConstituent(clauses.get(i));
 			displayChildren(clauses.get(i), box);
 			panel.add(box);
 		}
 	}
-	
-	public void displayChildren(Constituent parentConstit, Box parentBox)
-	{
+
+	public void displayChildren(Constituent parentConstit, Box parentBox) {
 		if (parentConstit == null)
 			return;
-		
+
 		ConstitList cList = parentConstit.getConstList();
-		for (int i = 0; i < cList.getLength(); i++)
-		{
+		for (int i = 0; i < cList.getLength(); i++) {
 			Constituent c = cList.getConstit(i);
 			Box box = new Box();
 			box.setConstituent(c);
 			parentBox.add(box);
 			displayChildren(c, box);
 		}
-		
+
 	}
-	
-	public JPanel getPanel()
-	{
+
+	public JPanel getPanel() {
 		return panel;
 	}
 }
